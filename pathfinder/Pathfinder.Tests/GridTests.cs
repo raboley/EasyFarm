927718304c61187cbee25f0d348c0176941deb44
Russell Boley <raboley@gmail.com>
2020-10-25 20:53:50 -0700

    idk?

---
+++ pathfinder/Pathfinder.Tests/GridTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyFarm.Pathfinding;
+using Xunit;
+
+
+namespace EasyFarm.Tests.Pathfinding
+{
+    
+    public class GridTests
+    {
+        [Fact]
+        public void CreateGridCanGenerateGridFromCorner()
+        {
+            // Arrange
+            Node[,] expectedGrid = new Node[3, 3];
+
+            // Act
+            // Assert
+
+        }
+    }
+}
