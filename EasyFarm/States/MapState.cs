d6665f88d882108b8a8c303c5fe6ceae60730586
russell <raboley@gmail.com>
2020-11-03 08:18:16 -0800

    Can now record NPCs, but will duplicate moving npcs ad infinity. Also won't record anything but the all for some reason.

--- EasyFarm/States/MapState.cs
+++ EasyFarm/States/MapState.cs
@@ -22,20 +22,27 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Forms.VisualStyles;
 using System.Windows.Media.Media3D;
 using EasyFarm.Classes;
 using EasyFarm.Context;
+using EasyFarm.Parsing;
 using EasyFarm.UserSettings;
 using EasyFarm.ViewModels;
 using EasyFarm.Views;
 using MahApps.Metro.Controls;
 using MemoryAPI;
 using Pathfinder;
+using Pathfinder.Map;
+using Pathfinder.Pathing;
+using Pathfinder.People;
+using Pathfinder.Persistence;
 using Navigator = EasyFarm.Classes.Navigator;
 using Player = EasyFarm.Classes.Player;
 using Position = MemoryAPI.Navigation.Position;
+using Zone = MemoryAPI.Zone;
 
 namespace EasyFarm.States
 {
@@ -50,140 +57,223 @@ namespace EasyFarm.States
         }
 
         public override void Run(IGameContext context)
-        { 
-           LogViewModel.Write("Entered Mapping state");
-           IMemoryAPI fface = context.API;
-           context.Memory.EliteApi.Navigator.DistanceTolerance = 3;
-            var gridFactory = new GridFactory();
-           var persister = new FilePersister();
-           var mapsDirectory = GetMapsDirectory();
+        {
+            LogViewModel.Write("Entered Mapping state");
 
+            // Make a Zone
+            var persister = NewZoneMapPersister();
 
-           persister.DefaultExtension = "json";
-           persister.FilePath = mapsDirectory;
-           gridFactory.Persister = persister;
+            var zoneMapFactory = new ZoneMapFactory();
+            zoneMapFactory.Persister = persister;
+            zoneMapFactory.DefaultGridSize = new Vector2(600f, 600f);
 
-           gridFactory.DefaultGridSize = new Vector2(600f, 600f);
+            string mapName = context.Player.Zone.ToString();
+            var zoneMap = zoneMapFactory.LoadGridOrCreateNew(mapName);
 
-           string mapName = context.Player.Zone.ToString();
-           var zoneGrid = gridFactory.LoadGridOrCreateNew(mapName);
-           var pathfinder = new Pathfinding {Grid = zoneGrid};
+            var zone = new Pathfinder.Map.Zone(mapName);
+            zone.Map = zoneMap;
 
-
-           var test = zoneGrid.NpcList.Find(n => n.Name.Contains("I.M."));
-
-            AddNpcsToGrid(context, zoneGrid);
-           AddInanimateObjectsToGrid(context, zoneGrid);
-
-           List<Vector3> walkedNodes = new List<Vector3>();
-
-            while (zoneGrid.MapName == context.Player.Zone.ToString())
-           // for (int i = 0; i < 1000; i++)
-           {
-               var player = context.API.Player;
-               var myPosition = RoundPositionToVector3(player.Position); 
-               
-               // Go get signet
-               if (context.Player.CurrentGoal == "Signet")
-               {
-                   // Find Signet Guy
-                   if (zoneGrid.NpcList.Count == 0)
-                   {
-                       LogViewModel.Write("I need signet, but don't know where any NPCs are in this zone");
-                       break;
-                   }
-
-                   var signetPerson = zoneGrid.NpcList.Find(n => n.Name.Contains("I.M."));
-
-                   if (signetPerson == null)
-                   {
-                       LogViewModel.Write("I need signet, but can't find the signet person in this zone");
-                       break;
-                   }
-
-                   LogViewModel.Write("Found signet person:" + signetPerson.Name + " At position: " + signetPerson.Position + " Headed there now");
-                   // Set route to the path to the signet guy
-                   var path = pathfinder.FindWaypoints(myPosition, signetPerson.Position);
-                   var obsWaypoints = ConvertVectorArrayToObservableCollectionPosition(path);
-                   context.Config.Route.Waypoints = obsWaypoints;
-
-                    //// Actually walking
-                    Navigator.StartRoute(context);
-
-                    // Got to the signet person
-                    LogViewModel.Write("Made it to signet person!");
-                    Config.Instance.Route.Reset();
-                    context.API.Navigator.Reset();
-
-                   // Get signet
-
-
-                   // Got Signet, reset goal
-                   context.Player.CurrentGoal = "Aimless";
-
-               }
-                   
-
-               
-
-
-
-               if (!walkedNodes.Contains(myPosition))
-               {
-
-
-                    walkedNodes.Add(myPosition);
-                   zoneGrid.AddKnownNode(myPosition);
-                   LogViewModel.Write("in map: " + zoneGrid.MapName + " Adding position: " + myPosition);
-               }
-           }
             
-            var lastPositionBeforeZone = RoundPositionToVector3(context.API.Player.Position);
-            if (lastPositionBeforeZone != Vector3.Zero)
-            {
-                while (context.API.Player.Zone == Zone.Unknown)
-                {
-                    TimeWaiter.Pause(100);
-                }
-                zoneGrid.AddZoneBoundary(context.Player.Zone.ToString(), lastPositionBeforeZone);
-            }
+            var mapNpcPersister = WatchNpcsInZone(mapName, zone);
 
+            
 
-            // // Map the NPCs
-           // foreach (var unit in context.Units)
-           // {
-           //     Vector3 position = new Vector3(unit.PosX, unit.PosY, unit.PosZ);
-           //     var npc = new NPC(unit.Name, position);
-           //     npc.ID = unit.Id;
-           //
-           //      if (unit.NpcType == NpcType.NPC) 
-           //          zoneGrid.AddNpc(npc);
-           //
-           //      // Map the objects
-           //      if (unit.NpcType == NpcType.InanimateObject)
-           //          zoneGrid.AddInanimateObject(npc);
-           //
-           //
-           //      // Map the Mobs
-           //
-           // }
+            ///// Setup the ALL person watcher
+            // var allNpcPersister = new Pathfinder.Persistence.FilePersister();
+            // allNpcPersister.FilePath = GetAndCreateDirectorFromRoot("NPCs");
+            // allNpcPersister.FileName = "all";
+            //
+            // var allPersonActor = new PersonActor();
+            // allPersonActor.Persister = allNpcPersister;
+            //
+            // //Watch the NPC collection of the Zone
+            // var allPeopleWatcher = new CollectionWatcher<Person>(zone.Npcs, allPersonActor);
 
+            // On Change Save the zone's NPCs to a file for that zone's NPCs
+            LogViewModel.Write("Adding NPCs to Zone");
+            AddNpcsToGrid(context, zone);
+            LogViewModel.Write("Done!");
 
-           
-           
-           gridFactory.Persister.Save(zoneGrid);
-
-           // write zones
-
-           // Discover all undiscovered nodes.
-
-           // Console.WriteLine(zoneGrid.Print());
-
+            // Also Save to the ALL NPC file
         }
 
+        private static FilePersister WatchNpcsInZone(string mapName, Pathfinder.Map.Zone zone)
+        {
+            var mapNpcPersister = WatchAndPersistNpcs(mapName, zone);
+            var allNpcPersister = WatchAndPersistNpcs("all", zone);
+
+            return mapNpcPersister;
+        }
+
+        private static FilePersister WatchAndPersistNpcs(string fileName, Pathfinder.Map.Zone zone)
+        {
+            // Setup the PersonActor to save to file
+            var npcPersister = new Pathfinder.Persistence.FilePersister();
+            npcPersister.FilePath = GetAndCreateDirectorFromRoot("NPCs");
+            npcPersister.FileName = fileName;
+
+            zone.LoadNpcsOrCreateNew(npcPersister);
+            
+            var personActor = new PersonActor();
+            personActor.Persister = npcPersister;
+
+            //Watch the NPC collection of the Zone
+            var peopleWatcher = new CollectionWatcher<Person>(zone.Npcs, personActor);
+            return npcPersister;
+        }
+
+        private static FilePersister NewZoneMapPersister()
+        {
+            var persister = new FilePersister();
+            var mapsDirectory = GetMapsDirectory();
+            persister.DefaultExtension = "json";
+            persister.FilePath = mapsDirectory;
+            return persister;
+        }
+
+        // public override void Run(IGameContext context)
+        // {
+        //     LogViewModel.Write("Entered Mapping state");
+        //     IMemoryAPI fface = context.API;
+        //     context.Memory.EliteApi.Navigator.DistanceTolerance = 3;
+        //     var gridFactory = new ZoneMapFactory();
+        //     var persister = new FilePersister();
+        //     var mapsDirectory = GetMapsDirectory();
+        //
+        //
+        //     persister.DefaultExtension = "json";
+        //     persister.FilePath = mapsDirectory;
+        //     gridFactory.Persister = persister;
+        //
+        //     gridFactory.DefaultGridSize = new Vector2(600f, 600f);
+        //
+        //     string mapName = context.Player.Zone.ToString();
+        //     var zoneGrid = gridFactory.LoadGridOrCreateNew(mapName);
+        //
+        //
+        //     var test = zoneGrid.NpcList.Find(n => n.Name.Contains("I.M."));
+        //
+        //     AddNpcsToGrid(context, zoneGrid);
+        //     AddInanimateObjectsToGrid(context, zoneGrid);
+        //
+        //     List<Vector3> walkedNodes = new List<Vector3>();
+        //
+        //     while (zoneGrid.MapName == context.Player.Zone.ToString())
+        //         // for (int i = 0; i < 1000; i++)
+        //     {
+        //         var player = context.API.Player;
+        //         var myPosition = RoundPositionToVector3(player.Position);
+        //
+        //         // Go get signet
+        //         if (context.Player.CurrentGoal == "Signet")
+        //         {
+        //             // Find Signet Guy
+        //             if (zoneGrid.NpcList.Count == 0)
+        //             {
+        //                 LogViewModel.Write("I need signet, but don't know where any NPCs are in this zone");
+        //                 break;
+        //             }
+        //
+        //             var signetPerson = zoneGrid.NpcList.Find(n => n.Name.Contains("I.M."));
+        //
+        //             if (signetPerson == null)
+        //             {
+        //                 LogViewModel.Write("I need signet, but can't find the signet person in this zone");
+        //                 break;
+        //             }
+        //
+        //             LogViewModel.Write("Found signet person:" + signetPerson.Name + " At position: " +
+        //                                signetPerson.Position + " Headed there now");
+        //             // Set route to the path to the signet guy
+        //             var path = Pathfinder.Pathing.Pathfinding.FindWaypoints(zoneGrid, myPosition, signetPerson.Position);
+        //             var obsWaypoints = ConvertVectorArrayToObservableCollectionPosition(path);
+        //             context.Config.Route.Waypoints = obsWaypoints;
+        //
+        //             //// Actually walking
+        //             Navigator.StartRoute(context);
+        //
+        //             // Got to the signet person
+        //             LogViewModel.Write("Made it to signet person!");
+        //             Config.Instance.Route.Reset();
+        //             context.API.Navigator.Reset();
+        //
+        //             // Get signet
+        //
+        //
+        //             // Got Signet, reset goal
+        //             context.Player.CurrentGoal = "Aimless";
+        //         }
+        //
+        //
+        //         if (!walkedNodes.Contains(myPosition))
+        //         {
+        //             walkedNodes.Add(myPosition);
+        //             zoneGrid.AddKnownNode(myPosition);
+        //             LogViewModel.Write("in map: " + zoneGrid.MapName + " Adding position: " + myPosition);
+        //         }
+        //     }
+        //
+        //     var lastPositionBeforeZone = RoundPositionToVector3(context.API.Player.Position);
+        //     if (lastPositionBeforeZone != Vector3.Zero)
+        //     {
+        //         while (context.API.Player.Zone == Zone.Unknown)
+        //         {
+        //             TimeWaiter.Pause(100);
+        //         }
+        //
+        //         zoneGrid.AddZoneBoundary(context.Player.Zone.ToString(), lastPositionBeforeZone);
+        //     }
+        //
+        //
+        //     // // Map the NPCs
+        //     // foreach (var unit in context.Units)
+        //     // {
+        //     //     Vector3 position = new Vector3(unit.PosX, unit.PosY, unit.PosZ);
+        //     //     var npc = new NPC(unit.Name, position);
+        //     //     npc.ID = unit.Id;
+        //     //
+        //     //      if (unit.NpcType == NpcType.NPC) 
+        //     //          zoneGrid.AddNpc(npc);
+        //     //
+        //     //      // Map the objects
+        //     //      if (unit.NpcType == NpcType.InanimateObject)
+        //     //          zoneGrid.AddInanimateObject(npc);
+        //     //
+        //     //
+        //     //      // Map the Mobs
+        //     //
+        //     // }
+        //
+        //
+        //     gridFactory.Persister.Save(zoneGrid);
+        //
+        //     // write zones
+        //
+        //     // Discover all undiscovered nodes.
+        //
+        //     // Console.WriteLine(zoneGrid.Print());
+        // }
 
 
         private static string GetMapsDirectory()
+        {
+            var repoRoot = GetRepoRoot();
+
+            repoRoot = Path.Combine(repoRoot, "Maps");
+            Directory.CreateDirectory(repoRoot);
+            return repoRoot;
+        }
+
+        private static string GetAndCreateDirectorFromRoot(string directory)
+        {
+            var repoRoot = GetRepoRoot();
+
+            repoRoot = Path.Combine(repoRoot, directory);
+            Directory.CreateDirectory(repoRoot);
+            return repoRoot; 
+        }
+
+        private static string GetRepoRoot()
         {
             string repoRoot = Directory.GetCurrentDirectory();
             for (int i = 0; i < 3; i++)
@@ -191,32 +281,31 @@ namespace EasyFarm.States
                 repoRoot = Directory.GetParent(repoRoot).FullName;
             }
 
-            repoRoot = Path.Combine(repoRoot, "Maps");
-            Directory.CreateDirectory(repoRoot);
             return repoRoot;
         }
 
-        private static void AddInanimateObjectsToGrid(IGameContext context, Grid zoneGrid)
+        private static void AddInanimateObjectsToGrid(IGameContext context, Pathfinder.Map.Zone zone)
         {
             foreach (var unit in context.Memory.UnitService.InanimateObjectsUnits)
             {
                 Vector3 pos = RoundPositionToVector3(unit.Position);
-                var thing = new NPC(unit.Name, pos);
-                zoneGrid.AddInanimateObject(thing);
+                var thing = new Person(unit.Id, unit.Name, pos);
+                zone.AddInanimateObject(thing);
             }
         }
 
-        private static void AddNpcsToGrid(IGameContext context, Grid zoneGrid)
+        private static void AddNpcsToGrid(IGameContext context, Pathfinder.Map.Zone zone)
         {
             foreach (var unit in context.Memory.UnitService.NpcUnits)
             {
                 Vector3 pos = RoundPositionToVector3(unit.Position);
-                var npc = new NPC(unit.Name, pos);
-                zoneGrid.AddNpc(npc);
+                var npc = new Person(unit.Id, unit.Name, pos);
+                zone.AddNpc(npc);
             }
         }
 
-        private ObservableCollection<MemoryAPI.Navigation.Position> ConvertVectorArrayToObservableCollectionPosition(Vector3[] path)
+        private ObservableCollection<MemoryAPI.Navigation.Position> ConvertVectorArrayToObservableCollectionPosition(
+            Vector3[] path)
         {
             var waypoints = new ObservableCollection<MemoryAPI.Navigation.Position>();
             for (int i = 0; i < path.Length; i++)
