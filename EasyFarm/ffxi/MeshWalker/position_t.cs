d4bf397510c95cc92d4fadd617b3d617e7f12007
russell <raboley@gmail.com>
2020-12-06 07:06:56 -0800

    Probably not gunna do this anymore.

---
+++ EasyFarm/ffxi/MeshWalker/position_t.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace EasyFarm.ffxi.MeshWalker
+{
+    public struct position_t
+    {
+        /// <summary>
+        /// The x
+        /// </summary>
+        public float X;
+
+        /// <summary>
+        /// The y
+        /// </summary>
+        public float Y;
+
+        /// <summary>
+        /// The z
+        /// </summary>
+        public float Z;
+
+        /// <summary>
+        /// The moving
+        /// </summary>
+        public UInt16 Moving;
+
+        /// <summary>
+        /// The rotation
+        /// </summary>
+        public sbyte Rotation;
+    }
+}
